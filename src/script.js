b3ba355708015771e31e2351d26734db8dcbae4d
matthewkuria <mathewkwachira@gmail.com>
2023-12-15 15:39:18 +0300

    add dropdown functionality to the drop downs

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
+// target the dropdown buttons, dropdown and arrow
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
+
+const toggleDropdown = function () {
+    dropdownMenu.classList.toggle("show");
+    toggleArrow.classList.toggle("arrow");
+  };
+
+//   add an eventlistener for the button
+dropdownBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+  });
+
 document.getElementById('hamburger').addEventListener('click', function () {
     document.getElementById('mobileMenu').classList.toggle('hidden');
 });
